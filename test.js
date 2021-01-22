5463320d5b5099683efd03004076b14c7c5955a0
mohit12611 <54572613+mohit12611@users.noreply.github.com>
2021-01-22 13:30:34 +0530

    ASSIGNMENT_TECH_SFECIFICTIONS

---
+++ test.js
@@ -0,0 +1,12 @@
+let TechnicalSpecifications = 
+ {
+     "Dimensions" : {"Overall_length" : 3500 , "Overall_Width": 1600 , "Overall_Height" : 1490 , "Overall_Wheelbase" : 2360 , "Track_width": {"Front(mm)" : 1405 , "Rear(mm)" : 1400 }, "Minimum_turning_radius(m)": 4.5 , "Minimum_ground_clearance(mm)": 170 },
+     "Capacities" : {"Seating_capacities" : 5, "Fuel_tank_capacities": 35}, 
+     "Engine": {"Type" : "KB-Series" , "Number_of_cylinder" : 3 , "Number_of_valves": 12 , "Capacity" : 998 , "Bore * Stroke" :  "73.0 * 79.5" , "Compression_ratio"  : "10:1" , "Maximum_power" : "67/6200" , "Maximum_torque": "90/3500"  ,"Fuel_distribution": "Multipoint injection"},
+     "Transmission" : "5-Speed MT" , 
+     "Chassis" : {"Steering" : "Rack & Pinion and Power assisted" , "Brakes" : { "Front" : "Ventilated_discs" , "Rear" : "Drums"} , "Suspension" : {"Front": "MacPherson struts & coil spring" , "Rear" : "Isolated trailing link & conil spring" }, "Shock_Absorber" : "Gas filled" , "Tyre" : "155/80R13"}, 
+     "Weights" : {"Kerb_weight" : "860-880" , "Gross_vehical_weight": 1320}
+}
+
+
+console.log(TechnicalSpecifications);
